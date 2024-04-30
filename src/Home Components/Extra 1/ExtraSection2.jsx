7e5121b7fa8a6422dc42ad33b5013e4ad3f678ef
Deptoha <tohakhan249156@gmail.com>
2024-04-30 23:26:51 +0600

    added some change

--- src/Home Components/Extra 1/ExtraSection2.jsx
+++ src/Home Components/Extra 1/ExtraSection2.jsx
@@ -2,7 +2,7 @@ import './Extra.css'
 import image1 from './Image/96634251_abstract watercolour winter landscape background design 1711.jpg'
 import image2 from './Image/autism-day-awareness-collage-style-with-people.jpg'
 import image3 from './Image/watercolor-painting-with-multi-colored-abstract-backgrounds-generative-ai.jpg'
-
+// ///
 
 const ExtraSection2 = () => {
     return (
@@ -16,6 +16,7 @@ const ExtraSection2 = () => {
                 <div className='mb-5'>
                     <img className='cursor-pointer imageBorder lg:w-[400px] lg:h-[350px]' src={image2} alt="" />
                 </div>
+                {/* /// */}
                 <div>
                     <img className='cursor-pointer imageBorder  lg:w-[400px] lg:h-[350px]' src={image3} alt="" />
                 </div>
